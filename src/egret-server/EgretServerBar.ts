5827a801cd7490b06bb93da1e02cb1ba746b5f7a
zhutie <zhu.tie@163.com>
2020-10-14 13:29:52 +0800

    右下角添加快捷egret编译调试状态栏 选择菜单中删除了重启调试（使用机会少，可被其他命令替代）

--- src/egret-server/EgretServerBar.ts
+++ src/egret-server/EgretServerBar.ts
@@ -1,10 +1,12 @@
 import * as vscode from 'vscode';
 import Listener from '../common/Listener';
 import EgretServer from './EgretServer';
+import * as helper from "../helper";
 import { EgretServiceExtStatus, EgretServiceStatus } from "../define";
 import { getLogger, Logger } from '../common/Logger';
 export default class EgretServerBar extends Listener {
     private statusBar: vscode.StatusBarItem;
+    private reCompileBar: vscode.StatusBarItem;
     private _status = EgretServiceStatus.Free;
     private _extStatus = EgretServiceExtStatus.Free;
     private logger: Logger;
@@ -12,10 +14,10 @@ export default class EgretServerBar extends Listener {
         super();
         this.logger = getLogger(this);
         this.logger.devlog("constructor")
-        const myCommandId = 'egret-helper.showEgretMenu';
-        this.addListener(vscode.commands.registerCommand(myCommandId, () => {
-            this.logger.devlog(`constructor receive cmd ${myCommandId}`)
-            let pickItems = ["编译", "编译调试", "重启调试", "重启", "同步default.res.json"];
+        const barCommandId = 'egret-helper.showEgretMenu';
+        const pickItems = ["$(server) 编译", "$(debug) 编译调试", "$(refresh) 重启", `$(sync) 同步[${helper.getConfigObj().egretResourceJsonPath}]`];
+        this.addListener(vscode.commands.registerCommand(barCommandId, () => {
+            this.logger.devlog(`constructor receive cmd ${barCommandId}`)
             vscode.window.showQuickPick(pickItems).then(result => {
                 this.logger.devlog(`constructor pick ${result}`)
                 switch (result) {
@@ -26,12 +28,9 @@ export default class EgretServerBar extends Listener {
                         vscode.commands.executeCommand("egret-helper.egretBuildAndDebug");
                         break;
                     case pickItems[2]:
-                        vscode.commands.executeCommand("egret-helper.egretRestartAndDebug");
-                        break;
-                    case pickItems[3]:
                         vscode.commands.executeCommand("egret-helper.egretRestart");
                         break;
-                    case pickItems[4]:
+                    case pickItems[3]:
                         vscode.commands.executeCommand("egret-helper.egretResSync");
                         break;
                 }
@@ -39,10 +38,15 @@ export default class EgretServerBar extends Listener {
         }));
 
         this.statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-        this.statusBar.command = myCommandId;
-        this.logger.devlog(`constructor register cmd=${myCommandId}`)
+        this.statusBar.command = barCommandId;
         this.statusBar.show();
         subscriptions.push(this.statusBar);
+
+        this.reCompileBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+        this.reCompileBar.command = "egret-helper.egretBuildAndDebug";
+        subscriptions.push(this.reCompileBar);
+
+        this.updateView();
     }
     public get status() {
         return this._status;
@@ -50,7 +54,7 @@ export default class EgretServerBar extends Listener {
     public set status(status: EgretServiceStatus) {
         this.logger.devlog(`status=${status}`)
         this._status = status;
-        this.updateBarTxt();
+        this.updateView();
     }
     public get extStatus() {
         return this._extStatus;
@@ -58,7 +62,27 @@ export default class EgretServerBar extends Listener {
     public set extStatus(exStatus: EgretServiceExtStatus) {
         this.logger.devlog(`extStatus=${exStatus}`)
         this._extStatus = exStatus;
+        this.updateView();
+    }
+    private updateView() {
         this.updateBarTxt();
+        this.updateReCompileBar();
+    }
+    private updateReCompileBar() {
+        if (this.status === EgretServiceStatus.Running) {
+            this.reCompileBar.show();
+        } else {
+            this.reCompileBar.hide();
+        }
+        switch (this.extStatus) {
+            case EgretServiceExtStatus.Building:
+            case EgretServiceExtStatus.Syncing:
+                this.reCompileBar.text = "$(repo-sync~spin) Egret编译调试"
+                break;
+            default:
+                this.reCompileBar.text = "$(debug) Egret编译调试"
+                break;
+        }
     }
     private updateBarTxt() {
         let _statusTxt: string, _extStatusTxt: string = "";
@@ -101,6 +125,9 @@ export default class EgretServerBar extends Listener {
         this.logger.devlog(`destroy`)
         if (this.statusBar) {
             this.statusBar.dispose();
+        }
+        if (this.reCompileBar) {
+            this.reCompileBar.dispose();
         }
     }
 }
